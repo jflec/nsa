20bdfc091e57b831301c0061bc9639aff95a806a
JFlec <josephflec@gmail.com>
2022-01-23 23:00:05 -0600

    womp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :brackets
   devise_for :users
-  root 'pages#index'
+
+  authenticated :user do 
+    root 'pages#index', as: :authenticated_root
+  end
+
+  root 'pages#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
